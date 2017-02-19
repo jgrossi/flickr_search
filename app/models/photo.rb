d34d0aaf39cf6eecf4d2fb79d40168358fca582b
Junior Grossi <juniorgro@gmail.com>
2017-02-19 20:39:20 -0300

    Use raw search instead of encoded URL

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,6 @@
 class Photo < ApplicationRecord
   def self.search(query)
-    list = flickr.photos.search(text: URI::encode(query))
+    list = flickr.photos.search(text: query)
     list.map { |photo| FlickRaw::url_q(photo) }
   end
 end
