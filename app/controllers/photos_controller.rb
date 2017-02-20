f25fce3ca5f5033d9fbce196a46dd03dfcf76025
Junior Grossi <juniorgro@gmail.com>
2017-02-20 11:29:44 -0300

    Adjust layout to use form to create the photo

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,8 +1,10 @@
 class PhotosController < ApplicationController
   def index
+    @photo = Photo.new
   end
 
   def search
     @photos = Photo.search(params[:q])
+    @photo = Photo.new
   end
 end
