8531918c38728e1c9f4664b0330e9790fca05324
Junior Grossi <juniorgro@gmail.com>
2017-02-20 18:04:16 -0300

    Add tests cases for photos controller

--- spec/controllers/photos_controller_spec.rb
+++ spec/controllers/photos_controller_spec.rb
@@ -13,4 +13,23 @@ RSpec.describe PhotosController, type: :controller do
       expect(response).to render_template(:search)
     end
   end
+
+  describe "POST #create" do
+    let(:params) { { photo: { farm: 1, server: 2, flickr_id: '123', secret: 'secret' } } }
+    it "is an AJAX request" do
+      post :create, xhr: true, params: params
+      expect(response).to have_http_status(:success)
+    end
+
+    it "saves a new photo in the database" do
+      expect{ post :create, xhr: true, params: params }.to change(Photo, :count).by(1)
+    end
+  end
+
+  describe "DELETE #destroy" do
+    let(:params) { { photo: { farm: 1, server: 2, flickr_id: '123', secret: 'secret' } } }
+    it "removes a photo record from the database" do
+      expect { post :create, xhr: true, params: params }.to change(Photo, :count).by(1)
+    end
+  end
 end
