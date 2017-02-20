3a94d19b34ed52684dd7edc9c1edc7c91d01a9b5
Junior Grossi <juniorgro@gmail.com>
2017-02-20 09:27:31 -0300

    Add test cases for the Photo model

--- spec/controllers/photos_controller_spec.rb
+++ spec/controllers/photos_controller_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe PhotosController, type: :controller do
 
   describe "GET #search" do
     it "returns http success" do
-      get :search
+      get :search, params: { q: 'dog' }
       expect(response).to have_http_status(:success)
     end
 
     it "renders the search template" do
-      get :search
+      get :search, params: { q: 'dog' }
       expect(response).to render_template(:search)
     end
   end
--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Photo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'search for dog' do
+    it 'returns more than one result'
+    it 'returns a hash with the flickr results'
+    it 'has at least farm, server, id and secret fields'    
+  end
+
+  context 'image_src' do
+    it 'renders the a squared thumbnail passing the flickr hash'
+  end
+
+  context 'add image to library' do
+    it 'cannot be valid without a farm field'
+    it 'cannot be valid without a server field'
+    it 'cannot be valid without a flickr_id field'
+    it 'cannot be valid without a secret field'
+    it 'is valid with farm, server, flickr_id and secret fields'
+  end
+
+  it 'can return a hash with all fields to be used by the image_src method'
 end
