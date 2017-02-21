4c4a698cfa039740ba056f3b7c6aee759f2bd6e4
Junior Grossi <juniorgro@gmail.com>
2017-02-21 14:59:02 -0300

    Replace model class methods in favor of helpers

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -1,2 +1,9 @@
 module PhotosHelper
+  def photo_in_gallery?(id)
+    !Photo.where(flickr_id: id).blank?
+  end
+
+  def photo_thumbnail(photo)
+    FlickRaw::url_q(photo)
+  end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -11,18 +11,10 @@ class Photo < ApplicationRecord
     flickr.photos.search(text: query)
   end
 
-  def self.thumbnail(photo)
-    FlickRaw::url_q(photo)
-  end
-
   def build_url
     photo = self.dup
     photo.id = photo.flickr_id
     object = OpenStruct.new(photo.attributes)
     self.url = FlickRaw.url_q(object)
-  end
-
-  def self.in_gallery?(id)
-    !where(flickr_id: id).blank?
   end
 end
